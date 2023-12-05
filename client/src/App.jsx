3c89cddff0eb231b7f5149980c5b7586c32c0dfc
Lifecloud-qr <office@lifecloud-qr.com>
2023-12-05 21:59:12 +0200

    change of packageJson

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,53 +1,64 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import './App.css';  // Make sure to create this CSS file
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css"; // Make sure to create this CSS file
 
 function App() {
-    const [requestContent, setRequestContent] = useState('');
-    const [response, setResponse] = useState('');
-    const [errorInfo, setErrorInfo] = useState(null);
+  const [requestContent, setRequestContent] = useState("");
+  const [response, setResponse] = useState("");
+  const [errorInfo, setErrorInfo] = useState(null);
 
-    const handleInputChange = (event) => {
-        setRequestContent(event.target.value);
-    };
+  const handleInputChange = (event) => {
+    setRequestContent(event.target.value);
+  };
 
-    const sendRequest = async () => {
-        try {
-            const res = await axios.post('http://localhost:3000/filter-request', { content: requestContent });
-            setResponse(res.data);
-            setErrorInfo(null);
-        } catch (error) {
-            if (error.response) {
-                setErrorInfo(error.response.data);
-            } else {
-                setErrorInfo({ message: 'An unknown error occurred' });
-            }
-            setResponse('');
-        }
-    };
+  const sendRequest = async () => {
+    try {
+      const res = await axios.post(
+        "https://example-waf.onrender.com/filter-request",
+        { content: requestContent }
+      );
+      setResponse(res.data);
+      setErrorInfo(null);
+    } catch (error) {
+      if (error.response) {
+        setErrorInfo(error.response.data);
+      } else {
+        setErrorInfo({ message: "An unknown error occurred" });
+      }
+      setResponse("");
+    }
+  };
 
-    return (
-        <div className="app">
-            <h1>Malicious Request Detector</h1>
-            <textarea 
-                className="input-area" 
-                onChange={handleInputChange} 
-                value={requestContent} 
-                placeholder="Enter request content"
-            />
-            <button className="send-button" onClick={sendRequest}>Send Request</button>
-            {response && <div className="response">{response}</div>}
-            {console.log(errorInfo)}
-            {errorInfo && (
-                <div className="error-dashboard">
-                    <h2>Error Detected</h2>
-                    <p><strong>Message:</strong> {errorInfo.message || errorInfo}</p>
-                    <p><strong>IP:</strong> {errorInfo.ip}</p>
-                    <p><strong>Geolocation:</strong> {errorInfo.geoLocation}</p>
-                </div>
-            )}
+  return (
+    <div className="app">
+      <h1>Malicious Request Detector</h1>
+      <textarea
+        className="input-area"
+        onChange={handleInputChange}
+        value={requestContent}
+        placeholder="Enter request content"
+      />
+      <button className="send-button" onClick={sendRequest}>
+        Send Request
+      </button>
+      {response && <div className="response">{response}</div>}
+      {console.log(errorInfo)}
+      {errorInfo && (
+        <div className="error-dashboard">
+          <h2>Error Detected</h2>
+          <p>
+            <strong>Message:</strong> {errorInfo.message || errorInfo}
+          </p>
+          <p>
+            <strong>IP:</strong> {errorInfo.ip}
+          </p>
+          <p>
+            <strong>Geolocation:</strong> {errorInfo.geoLocation}
+          </p>
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
 export default App;
